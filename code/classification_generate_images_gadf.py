53322936a52e54890c3fb22b2d3f5753daeb3667
Òscar <osgaor@alumni.upv.es>
2022-12-23 22:57:48 +0100

    Update classification_generate_images_gadf.py

--- code/classification_generate_images_gadf.py
+++ code/classification_generate_images_gadf.py
@@ -13,38 +13,43 @@ import sys
 import os
 from shutil import copyfile, move
 
-
+# path to the files containing the anomalous diffusing trajectories, created using the andi-datasets python package
 task2 = 'task2.txt' 
 ref2 = 'ref2.txt' 
 
-# path to the files containing the anomalous diffusing trajectories, created using the andi-datasets python package
+# we read the file and convert to a numpy array
 with open(task2, "r") as f:
     task = [np.array(x.split(";"), dtype="float32") for x in f.readlines()]
     task = np.array([x[1:] for x in task]) 
     task = task[:,]
 
+# left padding with 0 to make all trajectories of same length (50)
 max_length = 50
 task = seqq.pad_sequences(task, maxlen=max_length, dtype="float32")
 
+# read the labels file
 with open(ref2, 'r') as f:
     ref = [float(x.split(';')[1].strip()) for x in f.readlines()[:]]
     ref_cat = to_categorical(ref)
 
+# instantiate a Gramian Angular Field with the difference method to convert the trajectories into images
 gadf = GramianAngularField(image_size=48, method='difference')
 X_gadf = gadf.fit_transform(task)
 
 len_task = len(task)
 
+# check that the directory to save the images exists or create it
 if not os.path.isdir('classification_images_gadf'):
     os.makedirs('classification_images_gadf', exist_ok=True)
 
+# iterate over the trajectories to create the images
 for i in range(len_task):
     print(f'\r{i}/{len_task} - {round(i/len_task*100, 2)}%', end='')
     plt.imshow(X_gadf[i], extent=[0, 1, 0, 1], cmap = 'coolwarm', aspect = 'auto',vmax=abs(X_gadf[i]).max(), vmin=-abs(X_gadf[i]).max())
     plt.savefig('classification_images_gadf/X_gadf_{}.jpg'.format(i), bbox_inches='tight')
     plt.close("all")
 
-
+# images have to be saved by categories in as many different directories as categories, we create the directory structure here
 dataset_home = 'classification_images_gadf/'
 labeldirs = ['class1/', 'class2/', 'class3/', 'class4/', 'class5/']
 for labldir in labeldirs:
@@ -52,6 +57,7 @@ for labldir in labeldirs:
         newdir = dataset_home + labldir
         os.makedirs(newdir, exist_ok=True)
 
+# move each trajectory to the correct directory
 src_directory = 'classification_images_gadf/'
 for file in [x for x in os.listdir(src_directory) if not x.startswith("class")]:
     src = src_directory + '/' + file
