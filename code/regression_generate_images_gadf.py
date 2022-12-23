bb060600d18d3f6c1f126e9a41d3d5fa5bd2c9c7
Òscar <osgaor@alumni.upv.es>
2022-12-23 22:52:23 +0100

    Update regression_generate_images_gadf.py

--- code/regression_generate_images_gadf.py
+++ code/regression_generate_images_gadf.py
@@ -7,22 +7,27 @@ from shutil import copyfile, move
 task2 = 'task1.txt' 
 ref2 = 'ref1.txt' 
 
+# we read the file and convert to a numpy array
 with open(task1, "r") as f:
     task = [np.array(x.split(";"), dtype="float32") for x in f.readlines()]
     task = np.array([x[1:] for x in task]) 
     task = task[:,]
 
+# left padding with 0 to make all trajectories of same length (50)
 max_length = 50
 task = seqq.pad_sequences(task, maxlen=max_length, dtype="float32")
 
+# instantiate a Gramian Angular Field with the difference method to convert the trajectories into images
 gadf = GramianAngularField(image_size=48, method='difference')
 X_gadf = gadf.fit_transform(task)
 
 len_task = len(task)
 
+# check that the directory to save the images exists or create it
 if not os.path.isdir('regression_images_gadf'):
     os.makedirs('regression_images_gadf', exist_ok=True)
 
+# iterate over the trajectories to create the images
 for i in range(len_task):
     print(f'\r{i}/{len_task} - {round(i/len_task*100, 2)}%', end='')
     plt.imshow(X_gadf[i], extent=[0, 1, 0, 1], cmap = 'coolwarm', aspect = 'auto',vmax=abs(X_gadf[i]).max(), vmin=-abs(X_gadf[i]).max())
